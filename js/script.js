317448a5b6d6b937b166dbaad672c73bfaaa7df3
Kristina Miakaeva <92623612+kmiakaeva@users.noreply.github.com>
2023-02-18 17:51:47 +0300

    Изменила форматирование кода

--- js/script.js
+++ js/script.js
@@ -1,44 +1,43 @@
-"use strict";
+'use strict';
 
-const inputTask = document.querySelector(".input-task"),
-    addButton = document.querySelector(".add-button"),
-    tasksWrap = document.querySelector(".tasks-wrap"),
-    deleteBtnTasks = document.querySelector(".delete-btn-tasks"),
-    activeTasksWrap = document.querySelector(".active-tasks__wrap");
+const inputTask = document.querySelector('.input-task');
+const addButton = document.querySelector('.add-button');
+const tasksWrap = document.querySelector('.tasks-wrap');
+const deleteButtonTasks = document.querySelector('.delete-button-tasks');
+const activeTasksWrap = document.querySelector('.active-tasks__wrap');
 
-let tasks = JSON.parse(localStorage.getItem("to-do-list"));
-
+let tasks = JSON.parse(localStorage.getItem('to-do-list')) || [];
 
 function createTask() {
-    const text = inputTask.value;
-    if (text) {
-        if (!tasks) {
-            tasks = [];
-        }
-        const taskDescr = {
-            name: text,
-            checked: false,
-        };
-        tasks.push(taskDescr);
-        localStorage.setItem("to-do-list", JSON.stringify(tasks));
-        inputTask.value = "";
-        showToDoList();
+  const text = inputTask.value;
+  if (text) {
+    if (!tasks) {
+      tasks = [];
     }
+    const taskDescr = {
+      name: text,
+      checked: false,
+    };
+    tasks.push(taskDescr);
+    localStorage.setItem('to-do-list', JSON.stringify(tasks));
+    inputTask.value = '';
+    showToDoList();
+  }
 }
 
 function showToDoList() {
-    let template = "";
-    if (tasks) {
-        tasks.forEach((task, id) => {
-            template += createTemplate(task.name, id);
-            render(template);
-        });
-    }
+  let template = '';
+  if (tasks) {
+    tasks.forEach((task, id) => {
+      template += createTemplate(task.name, id);
+      render(template);
+    });
+  }
 }
 showToDoList();
 
 function createTemplate(task, id) {
-    return `
+  return `
         <div class="task task-wrap">
             <div class="input-task">
                 <label for="${id}" class="check" >
@@ -56,101 +55,98 @@ function createTemplate(task, id) {
 }
 
 function render(template) {
-    activeTasksWrap.innerHTML = template;
-    addEventListeners();
-    tasksWrap.hidden = false;
+  activeTasksWrap.innerHTML = template;
+  addEventListeners();
+  tasksWrap.hidden = false;
 }
 
 function addEventListeners() {
-    const tasks = document.querySelectorAll(".task");
-    tasks.forEach(task => {
-        const checkInput = task.querySelector(".check__input");
-        const pencil = task.querySelector(".edit-text");
-        const bin = task.querySelector(".bin");
-        checkInput.addEventListener("change", completeTask);
-        pencil.addEventListener("click", onEditTextOfTask);
-        bin.addEventListener("click", deleteTask);
-    });
+  const tasks = document.querySelectorAll('.task');
+  tasks.forEach((task) => {
+    const checkInput = task.querySelector('.check__input');
+    const pencil = task.querySelector('.edit-text');
+    const bin = task.querySelector('.bin');
+    checkInput.addEventListener('change', completeTask);
+    pencil.addEventListener('click', onEditTextOfTask);
+    bin.addEventListener('click', deleteTask);
+  });
 }
 
-
 function completeTask(e) {
-    const tg = e.target;
-    const id = tg.dataset.id;
-    const check = tg.closest(".check");
-    const checkInput = check.querySelector(".check__input");
-    const task = tg.closest(".task");
-    const checkText = task.querySelector(".text");
-    if (checkInput.checked) {
-        tasks[id].checked = true;
-        checkInput.setAttribute("checked", "checked");
-        checkText.classList.add("task_completed"); // Зачеркиваем задачу
-    } else {
-        tasks[id].checked = false;
-        checkInput.removeAttribute("checked", "checked");
-        checkText.classList.remove("task_completed"); // Убираем зачеркивание
-    }
-    localStorage.setItem("to-do-list", JSON.stringify(tasks));
+  const tg = e.target;
+  const id = tg.dataset.id;
+  const check = tg.closest('.check');
+  const checkInput = check.querySelector('.check__input');
+  const task = tg.closest('.task');
+  const checkText = task.querySelector('.text');
+  if (checkInput.checked) {
+    tasks[id].checked = true;
+    checkInput.setAttribute('checked', 'checked');
+    checkText.classList.add('task_completed'); // Зачеркиваем задачу
+  } else {
+    tasks[id].checked = false;
+    checkInput.removeAttribute('checked', 'checked');
+    checkText.classList.remove('task_completed'); // Убираем зачеркивание
+  }
+  localStorage.setItem('to-do-list', JSON.stringify(tasks));
 }
 
 function onEditTextOfTask(e) {
-    const tg = e.target;
-    const task = tg.closest(".task");
-    const textInputInCheck = task.querySelector(".text");
-    const pencil = task.querySelector(".edit-text");
-    switch (textInputInCheck.getAttribute("contenteditable")) {
-        case "false":
-            textInputInCheck.setAttribute("contenteditable", "true");
-            setTheCursorPosition(e);
-            pencil.innerText = "Save";
-            pencil.classList.toggle("save-text");
-            break;
+  const tg = e.target;
+  const task = tg.closest('.task');
+  const textInputInCheck = task.querySelector('.text');
+  const pencil = task.querySelector('.edit-text');
+  switch (textInputInCheck.getAttribute('contenteditable')) {
+    case 'false':
+      textInputInCheck.setAttribute('contenteditable', 'true');
+      setTheCursorPosition(e);
+      pencil.innerText = 'Save';
+      pencil.classList.toggle('save-text');
+      break;
 
-        case "true":
-            textInputInCheck.setAttribute("contenteditable", "false");
-            pencil.innerText = "";
-            pencil.classList.toggle("save-text");
-            break;
-    }
+    case 'true':
+      textInputInCheck.setAttribute('contenteditable', 'false');
+      pencil.innerText = '';
+      pencil.classList.toggle('save-text');
+      break;
+  }
 }
 
 function setTheCursorPosition(e) {
-    const tg = e.target;
-    const task = tg.closest(".task");
-    const textInputInCheck = task.querySelector(".text");
-    const selection = window.getSelection();
-    const range = document.createRange();
-    range.selectNodeContents(textInputInCheck);
-    selection.removeAllRanges();
-    range.collapse(false);
-    selection.addRange(range);
+  const tg = e.target;
+  const task = tg.closest('.task');
+  const textInputInCheck = task.querySelector('.text');
+  const selection = window.getSelection();
+  const range = document.createRange();
+  range.selectNodeContents(textInputInCheck);
+  selection.removeAllRanges();
+  range.collapse(false);
+  selection.addRange(range);
 }
 
 function deleteTask(e) {
-    const tg = e.target;
-    const task = tg.closest(".task");
-    task.remove();
-    if (!(activeTasksWrap.children.length)) {
-        tasksWrap.hidden = "true";
-    }
+  const tg = e.target;
+  const task = tg.closest('.task');
+  task.remove();
+  if (!activeTasksWrap.children.length) {
+    tasksWrap.hidden = 'true';
+  }
 }
 
 function deleteAllTasks() {
-    const allTasks = document.querySelectorAll(".task");
-    allTasks.forEach(task => {
-        task.remove();
-    });
-    tasksWrap.hidden = "true";
+  const allTasks = document.querySelectorAll('.task');
+  allTasks.forEach((task) => {
+    task.remove();
+  });
+  tasksWrap.hidden = 'true';
 }
 
-
-const OnEnterInput = (e) => e.code === "Enter" && createTask();
+const OnEnterInput = (e) => e.code === 'Enter' && createTask();
 
 function onClickAddButton() {
-    createTask();
+  createTask();
 }
 
-
-inputTask.addEventListener("keyup", OnEnterInput);
-addButton.addEventListener("click", onClickAddButton);
-deleteBtnTasks.addEventListener("click", deleteAllTasks);
+inputTask.addEventListener('keyup', OnEnterInput);
+addButton.addEventListener('click', onClickAddButton);
+deleteButtonTasks.addEventListener('click', deleteAllTasks);
